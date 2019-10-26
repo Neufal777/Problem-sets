919d89be6972d1942ad30db76dd04dc8cf43abe1
naoufal dahouli <neufal777@gmail.com>
2019-10-26 12:32:54 +0200

    uploading solution

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
+import "fmt"
+
+func whatNumber(num string) {
+
+	nameOfNumbers := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	fmt.Println(nameOfNumbers[num])
+
+}
+
 func main() {
 
-	//your solution here..
+	whatNumber("six")
 }
